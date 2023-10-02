4d5e9c245265a98ee58c9e44b84b9b9e8c1c90bc
MrAlfavista <pavelpetroff@bk.ru>
2023-10-02 17:17:21 +0300

    fixed

--- weather/src/main/java/org/weather/Weather.java
+++ weather/src/main/java/org/weather/Weather.java
@@ -1,49 +1,31 @@
 package org.weather;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
+import java.util.HashMap;
 
-
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Weather {
+    private static HashMap<String, Integer> regionsToId = new HashMap<>();
     private int id;
     private String region;
     private int temperature;
     private LocalDateTime date;
 
-    public Weather(int id, String region, int temperature, LocalDateTime date) {
-        this.id = id;
+    public Weather(String region, int temperature, LocalDateTime date) {
+        if(regionsToId.containsKey(region)){
+            this.id = regionsToId.get(region);
+        } else {
+            this.id = regionsToId.size();
+            regionsToId.put(region, this.id);
+        }
         this.region = region;
         this.temperature = temperature;
-        this.date = date;
-    }
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getRegion() {
-        return region;
-    }
-
-    public void setRegion(String region) {
-        this.region = region;
-    }
-
-    public int getTemperature() {
-        return temperature;
-    }
-
-    public void setTemperature(int temperature) {
-        this.temperature = temperature;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
         this.date = date;
     }
 
